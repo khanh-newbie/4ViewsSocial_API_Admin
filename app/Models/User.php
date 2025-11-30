74ae928fe0e2e8935708ab943b30697e89d36b0f
quang-star <nguyenkimquang1612@gmail.com>
2025-11-30 17:05:19 +0700

    Features/api admin (#1)
    * api_admin
    
    * Update DashboardController and api
    
    ---------
    
    Co-authored-by: khanh-newbie <nguyenduykhanh121204@gmail.com>

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,47 +2,38 @@
 
 namespace App\Models;
 
-// use Illuminate\Contracts\Auth\MustVerifyEmail;
-use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Foundation\Auth\User as Authenticatable;
-use Illuminate\Notifications\Notifiable;
+use Illuminate\Database\Eloquent\Model;
 
-class User extends Authenticatable
+class User extends Model
 {
-    /** @use HasFactory<\Database\Factories\UserFactory> */
-    use HasFactory, Notifiable;
+    protected $table = 'users';
 
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var list<string>
-     */
     protected $fillable = [
-        'name',
-        'email',
-        'password',
+        'user_name', 'full_name', 'avatar_url', 'gmail', 'password',
+        'facebook_url', 'thread_url', 'instagram_url', 'bio',
+        'role', 'online_status', 'status', 'login_fail'
     ];
 
-    /**
-     * The attributes that should be hidden for serialization.
-     *
-     * @var list<string>
-     */
-    protected $hidden = [
-        'password',
-        'remember_token',
-    ];
+    public $timestamps = true;
 
-    /**
-     * Get the attributes that should be cast.
-     *
-     * @return array<string, string>
-     */
-    protected function casts(): array
+    /* --- Relationships --- */
+    public function posts()
     {
-        return [
-            'email_verified_at' => 'datetime',
-            'password' => 'hashed',
-        ];
+        return $this->hasMany(Post::class, 'user_id');
+    }
+
+    public function follows()
+    {
+        return $this->hasMany(Follow::class, 'user_id');
+    }
+
+    public function followers()
+    {
+        return $this->hasMany(Follow::class, 'following_id');
+    }
+
+    public function stories()
+    {
+        return $this->hasMany(Story::class, 'user_id');
     }
 }
